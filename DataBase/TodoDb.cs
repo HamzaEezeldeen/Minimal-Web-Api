e7b5e58076e0e8653520dfec8c406cfae7bd7b5a
HamzaEezeldeen <h.k409383429@gmail.com>
2023-08-06 14:29:44 +0300

    Add project files.

---
+++ DataBase/TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minimal_Web_Api.Models;
+
+namespace Minimal_Web_Api.DataBase;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+      : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
